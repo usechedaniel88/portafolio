a97d9be34b9df74af3645e0407e4cd19a42a91f7
Daniel <usechedaniel88@gmail.com>
2023-02-01 22:01:40 +0100

    actualizacion

--- index.js
+++
@@ -1,8 +0,0 @@
-
-
-const toggleButton = document.getElementById('toggleButton');
-const naviList = document.getElementById('navi-List');
-
-toggleButton.addEventListener('click', () =>{
-    naviList.classList.toggle('active');
-})
